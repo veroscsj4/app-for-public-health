da50d89508516fcd900c8f70bac27bbdf61d7081
s0588969 <veroscsj4@hotmail.com>
2024-01-25 18:48:05 +0100

    löschen von unnötigen Datei

--- Flutter/lib/src/constants/jsonLoad.dart
+++ Flutter/lib/src/constants/jsonLoad.dart
@@ -3,100 +3,112 @@ import 'dart:convert';
 import 'package:mobile_app_for_public_health/src/data/genome_variant_medicament.dart';
 import 'package:mobile_app_for_public_health/src/data/genome_variant.dart';
 import 'package:mobile_app_for_public_health/src/data/genome_variant_information.dart';
+
 //read json file from drug.json
-  Future<List<GeneVariantMedicament>> loadGeneVariantsMedicaments() async {
-    try {
-      String jsonString = await rootBundle.loadString('assets/data/drug.json');
-      List<dynamic> jsonList = json.decode(jsonString);
-      return jsonList
-          .map((json) => GeneVariantMedicament.fromJson(json))
-          .toList();
-    } catch (error) {
-      print('Catch Error loading gene variants: $error');
-      return [];
-    }
+Future<List<GeneVariantMedicament>> loadGeneVariantsMedicaments() async {
+  try {
+    String jsonString = await rootBundle.loadString('assets/data/drug.json');
+    List<dynamic> jsonList = json.decode(jsonString);
+    return jsonList
+        .map((json) => GeneVariantMedicament.fromJson(json))
+        .toList();
+  } catch (error) {
+    print('Catch Error loading gene variants: $error');
+    return [];
   }
+}
 
 //read json file from genomeVariant.json
-  Future<List<GeneVariant>> loadGeneVariants() async {
-    try {
-      String jsonString =
-          await rootBundle.loadString('assets/data/genomeVariant.json');
-      List<dynamic> jsonList = json.decode(jsonString);
-      return jsonList.map((json) => GeneVariant.fromJson(json)).toList();
-    } catch (error) {
-      print('Catch Error loading gene variants: $error');
-      return [];
-    }
+Future<List<GeneVariant>> loadGeneVariants() async {
+  try {
+    String jsonString =
+        await rootBundle.loadString('assets/data/genomeVariant.json');
+    List<dynamic> jsonList = json.decode(jsonString);
+    return jsonList.map((json) => GeneVariant.fromJson(json)).toList();
+  } catch (error) {
+    print('Catch Error loading gene variants: $error');
+    return [];
   }
-  //read json file from genomeVariantInformation.json
-  Future<List<GeneVariantInformation>> loadGeneVariantInformation() async {
-    try {
-      String jsonString =
-          await rootBundle.loadString('assets/data/genomeVariantInformation.json');
-      List<dynamic> jsonList = json.decode(jsonString);
-      return jsonList.map((json) => GeneVariantInformation.fromJson(json)).toList();
-    } catch (error) {
-      print('Catch Error loading gene variants: $error');
-      return [];
-    }
-  }
-  // Filter data between geneVariant and geneVariantsMedicaments
-  List<GeneVariantMedicament> filterGeneVariantsMedicaments(
-      List<GeneVariant> geneVariantList,
-      List<GeneVariantMedicament> geneVariantsMedicaments) {
-    List<GeneVariantMedicament> filteredData = [];
+}
 
-    for (var geneVariantMedicament in geneVariantsMedicaments) {
-      for (var geneVariant in geneVariantList) {
-        if (geneVariant.geneVariant == geneVariantMedicament.geneVariant) {
-          filteredData.add(geneVariantMedicament);
-          break;
-        }
+//read json file from genomeVariantInformation.json
+Future<List<GeneVariantInformation>> loadGeneVariantInformation() async {
+  try {
+    String jsonString = await rootBundle
+        .loadString('assets/data/genomeVariantInformation.json');
+    List<dynamic> jsonList = json.decode(jsonString);
+    return jsonList
+        .map((json) => GeneVariantInformation.fromJson(json))
+        .toList();
+  } catch (error) {
+    print('Catch Error loading gene variants: $error');
+    return [];
+  }
+}
+
+// Filter data between geneVariant and geneVariantsMedicaments
+List<GeneVariantMedicament> filterGeneVariantsMedicaments(
+    List<GeneVariant> geneVariantList,
+    List<GeneVariantMedicament> geneVariantsMedicaments) {
+  List<GeneVariantMedicament> filteredData = [];
+
+  for (var geneVariantMedicament in geneVariantsMedicaments) {
+    for (var geneVariant in geneVariantList) {
+      if (geneVariant.geneVariant == geneVariantMedicament.geneVariant) {
+        filteredData.add(geneVariantMedicament);
+        break;
       }
     }
-
-    return filteredData;
   }
-Future<Map<String, dynamic>?> getInformation(dynamic searchObject, String typeOfInformation) async {
-  if(typeOfInformation == "genome"){
-    print(searchObject);
+
+  return filteredData;
+}
+
+// Retrieve information about Medicine or Genome.
+// The function searches for an element between the two JSON files
+// (drug.json or genomeVariantInformation.json).
+
+Future<Map<String, dynamic>?> getInformation(
+    dynamic searchObject, String typeOfInformation) async {
+  if (typeOfInformation == "genome") {
     try {
-       List<GeneVariantInformation> geneVariantInformation = await loadGeneVariantInformation();
-       //print('hier!!!!' + searchObject);
-      for (var genome in geneVariantInformation) {   
-        //print(genome.geneVariant + '     ' + genome.information.join(',') + '    ' + genome.chromosome + '    ' + genome.link.join(','));   
+      List<GeneVariantInformation> geneVariantInformation =
+          await loadGeneVariantInformation();
+      for (var genome in geneVariantInformation) {
         if (searchObject != null && searchObject == genome.geneVariant) {
-           print('genomeVariant = '+genome.chromosome);
-          return {'information':genome.information.join(','), 'chromosome': genome.chromosome.toString()};
+          return {
+            'information': genome.information.join(','),
+            'chromosome': genome.chromosome.toString(),
+            'link': genome.link.join(','),
+          };
         }
       }
       // Return a default value or handle the case when the medicine is not found
-      return {'information':'Genome information not found'};
+      return {'information': 'Genome information not found'};
     } catch (error) {
       print('Error loading genome variants: $error');
       // Handle the error as needed
-      return {'information':'Error loading gene variants'};
+      return {'information': 'Error loading gene variants'};
     }
-  }else{
+  } else {
     try {
       List<GeneVariantMedicament> geneVariantsMedicaments =
           await loadGeneVariantsMedicaments();
 
       for (var medicine in geneVariantsMedicaments) {
         if (searchObject.toString() == medicine.drugs.toString()) {
-          return {'information':medicine.effectOnDrugResponse.join(',')};
+          return {
+            'information': medicine.effectOnDrugResponse.join(','),
+          };
         }
       }
 
       // Return a default value or handle the case when the medicine is not found
-      return {'information':'Medicine information not found'};
+      return {'information': 'Medicine information not found'};
     } catch (error) {
       print('Error loading gene variants: $error');
       // Handle the error as needed
-      return {'information':'Error loading gene variants'};
+      return {'information': 'Error loading gene variants'};
     }
   }
 }
-
-
