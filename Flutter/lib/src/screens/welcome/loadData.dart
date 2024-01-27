90d7dd2fdb0b9a1750283bac4ec5faf3a17bb090
s0588969 <veroscsj4@hotmail.com>
2024-01-27 21:36:29 +0100

    Unterseite Upload: Link

--- Flutter/lib/src/screens/welcome/loadData.dart
+++ Flutter/lib/src/screens/welcome/loadData.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:mobile_app_for_public_health/src/constants/styles.dart';
 import '../../constants/img_path.dart';
 
 class UploadPage extends StatefulWidget {
   const UploadPage({Key? key}) : super(key: key);
-
   @override
   UploadPageState createState() => UploadPageState();
 }
 
 class UploadPageState extends State<UploadPage> {
+  final Uri _url = Uri.parse('https://genomedicine.com');
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: primaryColor,
         title: Text(
           'Thank you for registering!',
           style: Theme.of(context)
@@ -28,12 +30,12 @@ class UploadPageState extends State<UploadPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Image(image: AssetImage(uploadPNG), height: height * 0.5),
+            Image(image: AssetImage(uploadPNG), height: height * 0.3),
             Column(
               children: [
                 Text(
                   "Upload your genome",
-                  style: Theme.of(context).textTheme.headlineMedium,
+                  style: Theme.of(context).textTheme.headlineSmall,
                   textAlign: TextAlign.center,
                 ),
                 Text(
@@ -41,15 +43,30 @@ class UploadPageState extends State<UploadPage> {
                   style: Theme.of(context).textTheme.bodySmall,
                   textAlign: TextAlign.center,
                 ),
+                SizedBox(height: 20),
                 Text(
                   "Please visit this site:",
                   style: Theme.of(context).textTheme.bodySmall,
                   textAlign: TextAlign.center,
                 ),
-                Text(
-                  "GenoMedicine.com",
-                  style: Theme.of(context).textTheme.bodySmall,
-                  textAlign: TextAlign.center,
+                SizedBox(height: 20),
+                InkWell(
+                  onTap: () {
+                    _launchUrl();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      'genomedicine.com',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -57,5 +74,13 @@ class UploadPageState extends State<UploadPage> {
         ),
       ),
     );
+  }
+
+  Future<void> _launchUrl() async {
+    if (await launchUrl(_url)) {
+      await launchUrl(_url);
+    } else {
+      throw 'Could not launch $_url';
+    }
   }
 }
